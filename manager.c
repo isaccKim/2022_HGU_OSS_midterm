a6fcf7ff3c2f31e3363d15a03cbfd64654bda36a
seokwonKim <s21900107@walab.handong.edu>
2022-04-24 14:29:33 +0900

    loadData

--- manager.c
+++ manager.c
@@ -54,12 +54,22 @@ void saveData(Product p[], int count){
 int loadData(Product *p){
 	int count=0;
 	FILE*fp;
-
+	fp = fopen("product.txt","rt");
+	if(fp == NULL)printf("파일없음\n");
+	else {
+		for(int i=0; i<100; i++){
+			fscanf(fp,"%s",p[i].name);
+			if(feof(fp))break;
+			fscanf(fp,"%d",&p[i].weight);
+			fscanf(fp,"%d",&p[i].price);
+			count++;
+		}
 	//파일 내용을 읽어와서 배열에 값 추가하기
 
 
 
 
 	printf("=> 로딩 성공!\n");
+	}
 	return count;
 }
